942130bbe01a376c1882fd40e590b06fe155fc25
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-09-27 16:52:17 +0200

    add picture and information about artist

--- app/src/components/Artist.jsx
+++ app/src/components/Artist.jsx
@@ -4,22 +4,26 @@ import css from './Artist.module.css';
 function Artist() {
   const [artist, setArtistName] = useState();
   const [artistPicture, setArtistPicture] = useState();
+  const [artistDescription, setArtistDescription] = useState();
 
   (async () => {
     let response = await fetch(
-      'https://yt-music-api.herokuapp.com/api/yt/artists/elvis'
+      'https://yt-music-api.herokuapp.com/api/yt/artist/UChgxarBUCnPJV871-46bJ2g'
     );
     let result = await response.json();
-    console.log(result.content[0].name);
-    setArtistName(result.content[0].name);
-    setArtistPicture(result.content[0].thumbnails[0].url);
+    setArtistName(result.name);
+    setArtistPicture(result.thumbnails[0].url);
+    setArtistDescription(result.description);
   })();
 
   return (
     <div>
       <h1 className={css.name}>{artist}</h1>
       <div className={css.picture}>
-        <img src={artistPicture} width='50%'></img>
+        <img src={artistPicture}></img>
+      </div>
+      <div className={css.description}>
+        <p>{artistDescription}</p>
       </div>
     </div>
   );
