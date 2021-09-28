6fbfad33d659fd9ae12ec15129910d4a261570dd
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-09-28 11:22:09 +0200

    Added searchbar functionality with ytube API

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
+import SearchBar from './components/SearchBar';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Ljudio</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ app/src/components/SearchBar.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+function SearchBar() {
+  const [searchInput, setInput] = useState('');
+  const [songs, setSongs] = useState();
+  const [currentVideoId, setCurrentVideoId] = useState();
+
+  async function searchSong() {
+    let response = await fetch(
+      'https://yt-music-api.herokuapp.com/api/yt/songs/' + searchInput
+    );
+    let result = await response.json();
+    console.log(result.content);
+    setSongs(result.content);
+  }
+
+  function songClick(song) {
+    console.log(song.name);
+    setCurrentVideoId(song.videoId);
+    console.log(song.artist.browseId);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="search songs"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={searchSong}>Search</button>
+      <hr />
+
+      {songs &&
+        songs.map((song) => (
+          <div onClick={() => songClick(song)}>{song.name}</div>
+        ))}
+    </div>
+  );
+}
+
+export default SearchBar;
