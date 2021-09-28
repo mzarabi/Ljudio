658015026441d79c356c9c33780d3bb0f25e009d
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-09-28 15:58:57 +0200

    add album component together with rest of artistinfo, codealong Rasmus

--- app/src/components/Artist.jsx
+++ app/src/components/Artist.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Artist.module.css';
-import ArtistAlbums from './ArtistAlbums';
 
 function Artist() {
   const [artist, setArtistName] = useState();
   const [artistPicture, setArtistPicture] = useState();
   const [artistDescription, setArtistDescription] = useState();
+  const [artistAlbums, setArtistAlbums] = useState([]);
 
-  (async () => {
+  useEffect(() => {
+    getArtistApi();
+  }, []);
+
+  async function getArtistApi() {
     let response = await fetch(
-      'https://yt-music-api.herokuapp.com/api/yt/artist/UCGexNm_Kw4rdQjLxmpb2EKw'
+      'https://yt-music-api.herokuapp.com/api/yt/artist/UChgxarBUCnPJV871-46bJ2g'
     );
     let result = await response.json();
+
+    let albums = result.products.albums.content;
+    for (let i = 0; i < albums.length; i++) {
+      let albumPicture = albums[i].thumbnails[0].url;
+      setArtistAlbums((artistAlbums) => [...artistAlbums, albumPicture]);
+    }
+
     setArtistName(result.name);
     setArtistPicture(result.thumbnails[0].url);
     setArtistDescription(result.description);
-  })();
+  }
 
   return (
     <div>
@@ -25,6 +36,11 @@ function Artist() {
       </div>
       <div className={css.description}>
         <p>{artistDescription}</p>
+      </div>
+      <div>
+        {artistAlbums.map((picture, i) => (
+          <img src={picture} key={i}></img>
+        ))}
       </div>
     </div>
   );
