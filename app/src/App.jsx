2866d921ae9e91eb596c444f7adaf3789e0a010a
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-09-30 20:58:11 +0200

    Added player functionality
    Added the ytube api and we are now able to play songs from the search result list.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import React, { useState, createContext } from 'react';
 import './App.css';
 
 // import routing components
@@ -16,35 +16,39 @@ import searchIcon from './images/search.png';
 import playListIcon from './images/library.png';
 
 export const ContextArtistId = createContext();
+export const ContextVideoId = createContext();
 
 function App() {
   const [contextVal, setContext] = useState();
+  const [contextPlayerVal, setContextPlayer] = useState();
 
   return (
     <ContextArtistId.Provider value={[contextVal, setContext]}>
-      <div className='App'>
-        <Router>
-          <nav className='bottom-nav'>
-            <Link to='/'>
-              <img src={homeIcon} height={50} width={50} />
-            </Link>
+      <ContextVideoId.Provider value={[contextPlayerVal, setContextPlayer]}>
+        <div className="App">
+          <Router>
+            <nav className="bottom-nav">
+              <Link to="/">
+                <img src={homeIcon} height={50} width={50} />
+              </Link>
 
-            <Link to='/search'>
-              <img src={searchIcon} height={50} width={50} />
-            </Link>
+              <Link to="/search">
+                <img src={searchIcon} height={50} width={50} />
+              </Link>
 
-            <Link to='/artist'>
-              <img src={playListIcon} height={40} width={50} />
-            </Link>
-          </nav>
+              <Link to="/artist">
+                <img src={playListIcon} height={40} width={50} />
+              </Link>
+            </nav>
 
-          <main>
-            <Route path='/' exact component={StartPage} />
-            <Route path='/search' exact component={SearchPage} />
-            <Route path='/artist' exact component={ArtistPage} />
-          </main>
-        </Router>
-      </div>
+            <main>
+              <Route path="/" exact component={StartPage} />
+              <Route path="/search" exact component={SearchPage} />
+              <Route path="/artist" exact component={ArtistPage} />
+            </main>
+          </Router>
+        </div>
+      </ContextVideoId.Provider>
     </ContextArtistId.Provider>
   );
 }
