c7cf4190ed8fff3572ebfa2369829edf3901c8da
Arsalan Habib <71399565+arsalanhabib01@users.noreply.github.com>
2021-10-11 17:31:48 +0200

    Styling panel
    positioning play/pause panel

--- app/src/pages/StartPage.jsx
+++ app/src/pages/StartPage.jsx
@@ -4,8 +4,11 @@ import css from '../components/Styling.module.css';
 
 function StartPage() {
   return (
-    <div className={css.searchBar}>
-      <h1 className={css.title}>WELCOME</h1>
+    <div>
+      <h1 className={css.title}>WELCOME
+      </h1>
+      
+      <div className={css.searchBar}>
 
       <a href= '\search'>
           <input
@@ -13,6 +16,7 @@ function StartPage() {
           placeholder='Artists or songs'
         />
         </a>
+        </div>
     
     </div>
   );
