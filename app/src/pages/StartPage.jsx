457100a551c3de6f71da4aa4a6f74527781e440b
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-10-15 09:30:15 +0200

    change title on welcome screen

--- app/src/pages/StartPage.jsx
+++ app/src/pages/StartPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import css from '../components/Styling.module.css';
-import StartAlbums from '../components/StartAlbums'
+import StartAlbums from '../components/StartAlbums';
 
 function StartPage() {
   return (
     <div>
-      <h1 className={css.title}>WELCOME</h1>
-      <StartAlbums/>
+      <h1 className={css.title}>LJUDIO</h1>
+      <StartAlbums />
     </div>
   );
 }
